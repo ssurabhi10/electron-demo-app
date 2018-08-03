7713508a837d4d7181bc528cf2b7dadcacb16846
Surabhi Sharma <surabhisharma.226005@gmail.com>
2018-08-04 01:48:41 +0530

    A small app for electron demo

---
+++ src/js/add.js
@@ -0,0 +1,19 @@
+const electron = require('electron');
+const remote = electron.remote;
+const ipc = electron.ipcRenderer;
+
+const closeBtn = document.getElementById('closeBtn');
+
+closeBtn.addEventListener('click', function (event) {
+  const window = remote.getCurrentWindow();
+  window.close();
+})
+
+const updateBtn = document.getElementById('updateBtn');
+
+updateBtn.addEventListener('click', () => {
+  ipc.send('update-notify-value', document.getElementById('notifyVal').value);
+
+  const window = remote.getCurrentWindow();
+  window.close();
+});
